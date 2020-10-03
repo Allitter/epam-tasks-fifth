0e0677dea9ed16785199da4e1e8b6e7dcf976acb
Allitter <39375740+Allitter@users.noreply.github.com>
2020-10-03 12:22:05 +0300

    drop readerFactory selector

--- src/main/java/com/epam/tasks/fifth/Main.java
+++ src/main/java/com/epam/tasks/fifth/Main.java
@@ -4,8 +4,8 @@ package com.epam.tasks.fifth;
 // Программа обработки текста, который может быть получен как с консоли, так и с файла.
 // 5. Из текста удалить все слова заданной длины, начинающиеся на согласную букву.
 
-import com.epam.tasks.fifth.data.LinesReaderFactory;
-import com.epam.tasks.fifth.data.exceptions.FactoryNotExistsException;
+import com.epam.tasks.fifth.data.ConsoleLinesReaderFactory;
+import com.epam.tasks.fifth.data.ReaderFactory;
 import com.epam.tasks.fifth.data.input.LinesReader;
 import com.epam.tasks.fifth.domain.StringAsArrayProcessor;
 import com.epam.tasks.fifth.domain.StringProcessor;
@@ -16,9 +16,9 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, FactoryNotExistsException {
-        LinesReaderFactory factory = new LinesReaderFactory();
-        LinesReader reader = factory.createReader("console");
+    public static void main(String[] args) throws IOException {
+        ReaderFactory factory = new ConsoleLinesReaderFactory();
+        LinesReader reader = factory.createReader();
         String input = reader.readLines(2);
 
         StringProcessor processor = new StringAsArrayProcessor();
