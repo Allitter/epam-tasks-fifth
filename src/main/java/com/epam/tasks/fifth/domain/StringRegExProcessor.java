1be74b7cdefaab028169250eb0e3235704f7470c
Allitter <39375740+Allitter@users.noreply.github.com>
2020-10-07 20:05:38 +0300

    regex case insensitive

--- src/main/java/com/epam/tasks/fifth/domain/StringRegExProcessor.java
+++ src/main/java/com/epam/tasks/fifth/domain/StringRegExProcessor.java
@@ -6,18 +6,20 @@ import java.util.regex.Pattern;
 public class StringRegExProcessor implements StringProcessor {
     //language=RegExp
     private static final String CONSONANT_STARTING_WORD =
-            "(?i)\\b[a-z&&[^aeiou]][a-z]";
+            "[a-z&&[^aeiou]][a-z]";
 
     public String removeConsonantStartingWords(String line, int wordLength) {
         String toRemove = arrangeRegExWordLength(wordLength);
-        Pattern pattern = Pattern.compile(toRemove);
+        Pattern pattern = Pattern.compile(toRemove, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(line);
 
         return matcher.replaceAll("");
     }
 
-    private String arrangeRegExWordLength(int length) {
-        String result = String.format("%s{%d}\\b", CONSONANT_STARTING_WORD, length - 1);
+    private String arrangeRegExWordLength(int wordLength) {
+        String result = String.format("\\b%s{%d}\\b",
+                CONSONANT_STARTING_WORD, wordLength - 1);
+
         return result;
     }
 
